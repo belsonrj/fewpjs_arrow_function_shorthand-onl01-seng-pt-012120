68f591e41dd00807b4940de023eb948845e83626
Rick Belson <belsonrj@aol.com>
2020-08-14 01:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let divide = (num1, num2) => num1 / num2;
 divide(2000,100);
 
-const square = nums.map(x => x * x);
+let square = nums.map(x => x * x);
 
-const add = (num1, num2) => { num1 + num2 }
+let add = (num1, num2) => { num1 + num2 }
